9209e2e585f542866828f6f71d5b7870d97d131d
JuseongMoon <37950716+JuseongMoon@users.noreply.github.com>
2025-09-26 19:23:45 +0900

    Initial Commit

---
+++ ParkingFeeCalculatorLight/ParkingFeeCalculatorLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkingFeeCalculatorLightApp.swift
+//  ParkingFeeCalculatorLight
+//
+//  Created by 문주성 on 9/26/25.
+//
+
+import SwiftUI
+
+@main
+struct ParkingFeeCalculatorLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
